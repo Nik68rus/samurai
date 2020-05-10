50aef820e129d3a5a1d9d03d4a12a12d9fb29cd7
Nik68rus <nik200x@mail.ru>
2020-05-10 23:45:36 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+    <img src='https://www.seekpng.com/png/detail/177-1771840_logo-free-design-transparent-logo-design-png.png' alt='fff' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://i.simpalsmedia.com/999.md/BoardImages/900x900/8754611489eabcb08c41ce3cf6c31ce6.jpg' alt='wallpapers' />
+    </div>
+    <div>
+      Ava + description
+  </div>
+    <div>
+      My posts
+    <div>
+        New post
+    </div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+  </div>
+}
+
+export default Profile;
