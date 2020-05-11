b5516520e0875f211b6e5ab4519ef828e1c83fa9
Nik68rus <nik200x@mail.ru>
2020-05-12 00:30:16 +0300

    состояние после 21 урока

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,21 @@ import s from './Dialogs.module.scss';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogs__list}>
+        <div className={`${s.dialogs__toggle} ${s.active}`}>Sasha</div>
+        <div className={s.dialogs__toggle}>Masha</div>
+        <div className={s.dialogs__toggle}>Misha</div>
+        <div className={s.dialogs__toggle}>Dima</div>
+        <div className={s.dialogs__toggle}>Olya</div>
+      </div>
+      <div className={s.dialogs__items}>
+        <div className={s.dialogs__dialog}>Hi! Let's meet tomorrow!</div>
+        <div className={s.dialogs__dialog}>Where have you been???</div>
+        <div className={s.dialogs__dialog}>Hola amigo!</div>
+        <div className={s.dialogs__dialog}>Perfect weather</div>
+        <div className={s.dialogs__dialog}>Cmon</div>
+      </div>
     </div>
   );
 };
